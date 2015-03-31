4c0cd630dde381b8480d0374d71c5b572f90a54b
Anton Sokolchenko <wsevendays@gmail.com>
2015-03-31 21:08:16 +0300

    Update gcc-arm-none-eabi-49.rb

--- gcc-arm-none-eabi-49.rb
+++ gcc-arm-none-eabi-49.rb
@@ -3,9 +3,8 @@ require 'formula'
 class GccArmNoneEabi49 < Formula
   homepage 'https://launchpad.net/gcc-arm-embdded'
   version '20141203'
-  url 'http://launchpadlibrarian.net/192227901/gcc-arm-none-eabi-4_9-2014q4-20141203-mac.tar.bz2'
-  sha1 '46767b358cc42ffe126aec68afb9d22bd0ef5beb'
-
+  url 'https://launchpadlibrarian.net/200700934/gcc-arm-none-eabi-4_9-2015q1-20150306-mac.tar.bz2'
+  sha1 'da07fd4edc09da8748b3a61252eed793059c138f'
   def install 
     ohai 'Copying binaries...'
     system 'cp', '-rv', 'arm-none-eabi', 'bin', 'lib', 'share', "#{prefix}/"
